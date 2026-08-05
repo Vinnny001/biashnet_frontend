6e15677e31ac6e9bd3ab94a757347dd25f4dc2f1
Vincent Ogweno <ogwenovincent00@gmail.com>
2026-08-05 18:03:21 +0300

    added base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
+    base: "./",
+    
     plugins: [react()],
     resolve: {
       alias: {
